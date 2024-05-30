60d254e119b3909604ba2efa2618070342d3f538
alexbolog <mbolog.alex@gmail.com>
2024-05-30 12:20:22 +0300

    comment cleanup

--- mocks/umbrella_oracle_mock/src/umbrella_oracle_mock.rs
+++ mocks/umbrella_oracle_mock/src/umbrella_oracle_mock.rs
@@ -3,7 +3,6 @@
 multiversx_sc::imports!();
 multiversx_sc::derive_imports!();
 
-/// An empty contract. To be used as a template when starting a new contract from scratch.
 #[multiversx_sc::contract]
 pub trait UmbrellaOracleMock {
     #[init]
